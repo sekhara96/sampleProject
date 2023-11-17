974214418b58c210de5b4c6f78bee53c3d58d273
Sekhar <gsekharavm96@gmail.com>
2023-11-17 15:14:03 +0530

    initial commit

---
+++ src/main/java/com/sekhar/startproject/ApiExample.java
@@ -0,0 +1,19 @@
+package com.sekhar.startproject;
+
+public class ApiExample {
+
+	public static void main(String[] args) {
+		
+				for(int i=1; i<=5; i++) {
+					for(int j=1; j<=i; j++) {
+						System.out.print("* ");
+					}
+					System.out.println();
+				}
+
+			}
+
+		}
+
+
+   
